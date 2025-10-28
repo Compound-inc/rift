abf7857746cf270a6bcb602280945ed9c875858f
Arisayyy <github.yo-yo825@passinbox.com>
2025-10-28 16:27:41 -0600

    Update layout metadata descriptions for consistency in language

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     template: "%s | Rift",
     default: "Rift",
   },
-  description: "Rift AI",
+  description: "Todas las IAs en un solo lugar",
   applicationName: "Rift",
   metadataBase: new URL(baseUrl),
   alternates: {
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
   },
   openGraph: {
     title: "Rift",
-    description: "Todas las IA en un solo lugar",
+    description: "Todas las IAs en un solo lugar",
     url: baseUrl,
     siteName: "Rift",
     type: "website",
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Rift",
-    description: "Todas las IA en un solo lugar",
+    description: "Todas las IAs en un solo lugar",
     images: ["/twitter-image.png"],
   },
 };
